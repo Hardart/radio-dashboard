b642fd857db7649f09831f0d10fce07f68e0dc38
Hardart <kaino@bk.ru>
2024-09-22 11:00:56 +0300

    feat: create new UI component - badge

---
+++ src/components/ui/hdBadge/hdBadge.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  text: string
+  type?: 'primary' | 'secondary' | 'success'
+}>()
+</script>
+
+<template>
+  <div class="hd-badge" :class="type && `hd-badge--${type}`">{{ text }}</div>
+</template>
+
+<style lang="scss" scoped src="./hdBadge.scss"></style>
