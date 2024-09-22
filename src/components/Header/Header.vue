1616e03f16dbae02eac53e115e2d2e25a075a679
Hardart <kaino@bk.ru>
2024-09-22 10:28:33 +0300

    feat: create new Header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HeaderLeft from './HeaderLeft/HeaderLeft.vue'
+import HdButton from '@ui/hdButton/hdButton.vue'
+</script>
+
+<template>
+  <header class="header">
+    <HdButton icon="menu" icon-class="header__menu-button" />
+    <h3>Header</h3>
+    <HeaderLeft />
+  </header>
+</template>
+
+<style lang="scss" scoped src="./Header.scss" />
