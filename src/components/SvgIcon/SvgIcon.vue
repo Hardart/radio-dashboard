704508161259a117113b7db4ead9911ca13e9ddb
Hardart <kaino@bk.ru>
2024-09-22 09:59:27 +0300

    feat: add icons and images, create icon render component, add base SVG styles

---
+++ src/components/SvgIcon/SvgIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+export type Icon =
+  | 'dashboard'
+  | 'news'
+  | 'category'
+  | 'music'
+  | 'team'
+  | 'gallery'
+  | 'images'
+  | 'bell'
+  | 'menu'
+  | 'person'
+  | 'lock'
+  | 'mail'
+
+const { icon } = defineProps<{
+  icon: Icon
+}>()
+
+const svg = defineAsyncComponent(() => import(`@icons/${icon}.svg`))
+</script>
+
+<template>
+  <component :is="svg" class="hd-icon" />
+</template>
