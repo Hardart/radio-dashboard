d8fb69c0e387fdb02b6a10e10eb1898caa4253bb
Hardart <kaino@bk.ru>
2024-09-22 10:04:05 +0300

    feat: create new Login Form component

---
+++ src/layers/loginViewLayer/views/components/LoginForm/LoginForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import HdButton from '@/components/ui/hdButton/hdButton.vue'
+import HdInput from '@/components/ui/hdInput/hdInput.vue'
+</script>
+
+<template>
+  <form class="login-form">
+    <h3 class="login-form__title">Войти</h3>
+    <HdInput class="login-form__input" icon="person" />
+    <HdInput class="login-form__input" icon="lock" />
+    <HdButton text="Войти" class="login-form__submit" />
+    <div class="login-form__description">
+      <RouterLink to="/">Забыли пароль?</RouterLink>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped src="./LoginForm.scss"></style>
