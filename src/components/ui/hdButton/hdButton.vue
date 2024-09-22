4eb6f6fa21947258a72ad6d12db3a55e12b2ce4a
Hardart <kaino@bk.ru>
2024-09-22 10:19:40 +0300

    feat: create new UI component - button

---
+++ src/components/ui/hdButton/hdButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import SvgIcon, { type Icon } from '@/components/SvgIcon/SvgIcon.vue'
+
+defineProps<{
+  text?: string
+  type?: 'primary'
+  icon?: Icon
+  iconClass?: string
+}>()
+</script>
+
+<template>
+  <button class="hd-button" :class="type && `hd-button--${type}`">
+    <SvgIcon v-if="icon" :icon :class="iconClass" />
+    {{ text }}
+  </button>
+</template>
+
+<style lang="scss" scoped src="./hdButton.scss"></style>
