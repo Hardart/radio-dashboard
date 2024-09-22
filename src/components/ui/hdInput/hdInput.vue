c669a25262e3c2916ecc2a698bcaaa864dba87b3
Hardart <kaino@bk.ru>
2024-09-22 10:03:21 +0300

    feat: create new UI component - input

---
+++ src/components/ui/hdInput/hdInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SvgIcon, { type Icon } from '@/components/SvgIcon/SvgIcon.vue'
+defineProps<{
+  icon?: Icon
+  flipIcon?: Icon
+}>()
+</script>
+
+<template>
+  <div class="hd-input">
+    <div class="hd-input__container">
+      <div class="hd-input__icon" v-if="icon">
+        <SvgIcon :icon />
+      </div>
+      <input
+        class="hd-input__field"
+        :class="[icon && 'hd-input__field--icon']"
+        type="text"
+      />
+      <label class="hd-input__label"></label>
+      <SvgIcon v-if="flipIcon" :icon="flipIcon" class="hd-input__flip-icon" />
+    </div>
+  </div>
+</template>
+
+<style scoped src="./hdInput.scss" />
