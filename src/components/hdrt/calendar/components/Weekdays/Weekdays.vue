d3080c3c23e9262bea27884f3f918ec4cc0f7b01
Hardart <kaino@bk.ru>
2024-10-28 16:09:17 +0300

    feat: add new Calendar component

---
+++ src/components/hdrt/calendar/components/Weekdays/Weekdays.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const weekdays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс']
+</script>
+
+<template>
+  <div class="calendar-weekdays">
+    <div class="first-letter:uppercase" v-for="day in weekdays">{{ day }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
