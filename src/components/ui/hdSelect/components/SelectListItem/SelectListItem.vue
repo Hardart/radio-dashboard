11d78b4a3e060f95e082d72bb8dc26dee92ee145
Hardart <kaino@bk.ru>
2024-10-28 15:40:32 +0300

    feat: add new UI Select component

---
+++ src/components/ui/hdSelect/components/SelectListItem/SelectListItem.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import type { Option } from '../../hdSelect.vue'
+
+defineProps<{
+  isActive?: boolean
+  option: Option
+}>()
+defineEmits(['onSelect'])
+</script>
+
+<template>
+  <li
+    class="hd-select-item"
+    :class="isActive && 'hd-select-item--active'"
+    @click="$emit('onSelect', option)"
+  >
+    <slot />
+  </li>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
