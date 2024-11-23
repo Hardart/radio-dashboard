02f21e3eb38ff179f2783fb61fc2b60ef572efce
Hardart <kaino@bk.ru>
2024-11-23 11:43:13 +0300

    feat: add TipTap, jwt-decode, defu packages and create HdEditor component with controllers and extensions

---
+++ src/components/editor/HdEditor.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import StarterKit from '@tiptap/starter-kit'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import { provide, reactive, watch } from 'vue'
+import { Image } from './extensions/Image'
+import ControllerBold from './controllers/Bold.vue'
+import ControllerHeading from './controllers/Heading.vue'
+import ControllerItalic from './controllers/Italic.vue'
+import ControllerFloatRight from './controllers/FloatRight.vue'
+import ControllerUploadImage from './controllers/UploadImage.vue'
+// import Underline from '@tiptap/extension-underline'
+const content = defineModel<string | undefined>({ required: true })
+
+const image = reactive({
+  src: '',
+  description: '',
+})
+
+watch(
+  () => image.src,
+  () => {
+    editor.value
+      ?.chain()
+      .focus()
+      .setImage({ src: 'http://localhost:3068' + image.src })
+      .run()
+  }
+)
+
+const editor = useEditor({
+  content: content.value,
+  extensions: [StarterKit, Image],
+  editorProps: {
+    attributes: {
+      class: 'hd-editor__textarea',
+    },
+  },
+  onUpdate() {
+    content.value = editor.value?.getText().trim()
+      ? editor.value?.getHTML()
+      : editor.value?.getText().trim()
+  },
+})
+
+provide('tiptap', editor)
+</script>
+
+<template>
+  <div class="hd-editor">
+    <div v-if="editor" class="hd-editor__controllers">
+      <ControllerHeading />
+      <ControllerBold />
+      <ControllerItalic />
+      <ControllerFloatRight />
+      <ControllerUploadImage v-model="image.src" />
+      <!-- <EditorUnderline /> -->
+      <!-- <EditorBlockquote /> -->
+      <!-- <EditorFloatLeft /> -->
+      <!-- <EditorFloatRight /> -->
+      <!-- <EditorResetFloat /> -->
+    </div>
+
+    <EditorContent
+      :editor="editor"
+      class="hd-editor__container"
+      @click="editor?.commands.focus()"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss" src="./HdEditor.scss" />
---
+++ src/components/editor/controllers/FloatLeft.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { inject } from 'vue'
+const editor = inject<any>('tiptap')
+</script>
+<template>
+  <UButton
+    square
+    variant="outline"
+    color="gray"
+    icon="i-material-symbols-format-image-left-rounded"
+    class="editor__button"
+    @click="
+      editor
+        .chain()
+        .focus()
+        .updateAttributes('image', { class: 'w-72 float-left not-prose mr-4' })
+        .run()
+    "
+  />
+</template>
