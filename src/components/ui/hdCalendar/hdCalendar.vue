cc2efe46578fca1c33fc80990a3f0e07bd3ecbef
Hardart <kaino@bk.ru>
2024-11-23 11:27:31 +0300

    refactor: add label option to HdCalendar component

--- src/components/ui/hdCalendar/hdCalendar.vue
+++ src/components/ui/hdCalendar/hdCalendar.vue
@@ -4,12 +4,16 @@ import HdButton from '../hdButton/hdButton.vue'
 import Calendar from '@/components/hdrt/calendar/Calendar.vue'
 import { ref } from 'vue'
 const date = defineModel<string>({ required: true })
+defineProps<{
+  label?: string
+}>()
 const isShow = ref(false)
 const toggleShowState = () => (isShow.value = !isShow.value)
 </script>
 
 <template>
   <div class="hd-calendar">
+    <span class="hd-calendar__label" v-if="label">{{ label }}</span>
     <HdButton
       class="hd-calendar__button"
       :text="format(date, 'short')"
