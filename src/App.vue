b54c23662b774f2831dc2649b5ba138790b7e65e
Hardart <kaino@bk.ru>
2024-11-23 11:17:19 +0300

    fixup! feat: add new Tooltip component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterView, useRoute } from 'vue-router'
 import DashboardLayout from '@/layouts/dashboardLayout.vue'
 import LoginLayout from '@/layouts/loginLayout.vue'
 import { computed } from 'vue'
+import HdTooltip from './components/ui/hdTooltip/hdTooltip.vue'
 import HdNotifications from './components/ui/hdNotification/hdNotifications.vue'
 
 const route = useRoute()
@@ -13,5 +14,6 @@ const isLogin = computed(() => route.name === 'login')
   <component :is="isLogin ? LoginLayout : DashboardLayout">
     <RouterView />
   </component>
+  <HdTooltip />
   <HdNotifications />
 </template>
