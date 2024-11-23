f4cb048fc7e16fddabb87bf5ddbdee1403a951b4
Hardart <kaino@bk.ru>
2024-11-23 11:32:13 +0300

    feat: add new create news item layer

---
+++ src/layers/newsItemCreateViewLayer/views/newsItemCreateView/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useNewsStore } from '@/store/useNewsStore'
+import { useNewsItemStore } from '@/store/useNewsItemStore'
+
+import ArticleForm from './components/ArticleForm/ArticleForm.vue'
+
+const newsStore = useNewsStore()
+
+const { tags, categories } = storeToRefs(useNewsItemStore())
+const { articleForm } = storeToRefs(newsStore)
+</script>
+
+<template>
+  <ArticleForm v-model="articleForm" :tags :categories />
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
