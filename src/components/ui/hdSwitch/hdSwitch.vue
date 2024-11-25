aa86745c6463414da69fc7bb5a2c0dbe23ab8abd
Hardart <kaino@bk.ru>
2024-11-25 22:16:09 +0300

    refactor: add label option to Switch

--- src/components/ui/hdSwitch/hdSwitch.vue
+++ src/components/ui/hdSwitch/hdSwitch.vue
@@ -1,11 +1,15 @@
 <script lang="ts" setup>
 const isOn = defineModel<boolean>()
 const toggleSwitch = () => (isOn.value = !isOn.value)
+defineProps<{ label?: string }>()
 </script>
 
 <template>
   <div class="hd-switch" :class="isOn && 'hd-switch--on'" @click="toggleSwitch">
-    <div class="hd-switch__handler"></div>
+    <span class="hd-switch__label" v-if="label">{{ label }}</span>
+    <div class="hd-switch__wrapper">
+      <div class="hd-switch__handler"></div>
+    </div>
   </div>
 </template>
 
