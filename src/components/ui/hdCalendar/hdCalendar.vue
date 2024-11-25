a39677e192850d77373461a90ddf3f698ea6c85c
Hardart <kaino@bk.ru>
2024-11-25 22:17:55 +0300

    refactor: change separate select components to universal select component

--- src/components/ui/hdCalendar/hdCalendar.vue
+++ src/components/ui/hdCalendar/hdCalendar.vue
@@ -3,21 +3,22 @@ import { format } from '@formkit/tempo'
 import HdButton from '../hdButton/hdButton.vue'
 import Calendar from '@/components/hdrt/calendar/Calendar.vue'
 import { ref } from 'vue'
+import { useClickOutside } from '@/composables/useClickOutside'
+import { useToggle } from '@/composables/useToggle'
 const date = defineModel<string>({ required: true })
-defineProps<{
-  label?: string
-}>()
-const isShow = ref(false)
-const toggleShowState = () => (isShow.value = !isShow.value)
+defineProps<{ label?: string }>()
+const [isShow, toggle] = useToggle()
+const $calendar = ref()
+useClickOutside($calendar, () => toggle(false))
 </script>
 
 <template>
-  <div class="hd-calendar">
+  <div class="hd-calendar" ref="$calendar">
     <span class="hd-calendar__label" v-if="label">{{ label }}</span>
     <HdButton
       class="hd-calendar__button"
       :text="format(date, 'short')"
-      @click="toggleShowState"
+      @click="toggle()"
     />
     <Teleport defer to=".hd-calendar">
       <Calendar v-model="date" v-if="isShow" class="hd-calendar__container" />
