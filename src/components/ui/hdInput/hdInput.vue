7b611b17e1dbfec6f2051b9f8379fa51083c674c
Hardart <kaino@bk.ru>
2024-11-26 13:51:47 +0300

    refactor: preparre HdInput component for combine with form

--- src/components/ui/hdInput/hdInput.vue
+++ src/components/ui/hdInput/hdInput.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Icon } from '@type/Icon'
 import SvgIcon from '@/components/SvgIcon/SvgIcon.vue'
+import { inject, type InputTypeHTMLAttribute } from 'vue'
 const inputValue = defineModel()
 defineProps<{
   icon?: Icon
@@ -10,25 +11,27 @@ defineProps<{
   name?: string
   placeholder?: string
   requried?: boolean
+  type?: InputTypeHTMLAttribute
 }>()
+
+const id = inject<string | undefined>('input-id', undefined)
 </script>
 
 <template>
   <div class="hd-input">
-    <label class="hd-input__label" :for="name" v-if="label">{{ label }} </label>
     <div class="hd-input__container">
       <span class="hd-input__icon" v-if="icon">
         <SvgIcon :icon :class="iconClass" />
       </span>
       <input
+        :type="type || 'text'"
         class="hd-input__field"
         :class="[icon && 'hd-input__field--icon']"
-        :id="name"
         :name
         :placeholder
-        type="text"
-        v-model="inputValue"
         :requried
+        :id
+        v-model="inputValue"
       />
       <SvgIcon v-if="flipIcon" :icon="flipIcon" class="hd-input__flip-icon" />
     </div>
