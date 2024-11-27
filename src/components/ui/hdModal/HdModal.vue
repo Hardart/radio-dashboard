2f24cbcd5274721b94b1fa8aefe10a6f03ad0974
Hardart <kaino@bk.ru>
2024-11-27 23:56:29 +0300

    feat: add new HdModal component

---
+++ src/components/ui/hdModal/HdModal.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+  isOpen: boolean
+}>()
+</script>
+
+<template>
+  <Teleport to=".dashboard" defer>
+    <Transition>
+      <div class="modal" v-if="isOpen">
+        <slot />
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style lang="scss" scoped src="./HdModal.scss" />
