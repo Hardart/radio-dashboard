02e12a9b66dbcab68e74ca916013e320f5c71c30
Hardart <kaino@bk.ru>
2024-11-28 00:00:25 +0300

    feat: add enum type for url param in upload api

--- src/api/upload-api.ts
+++ src/api/upload-api.ts
@@ -1,11 +1,13 @@
 import type { ResponseApi } from '@/shared/types/ResponseAPI'
 import { useHdFetch } from './base-fetch/base-fetch'
+import { UploadPath } from '@/shared/enums/upload-path-url'
 
 export const filesAPI = {
-  async single(body: FormData) {
-    const { data } = await useHdFetch<ResponseApi.FileSingle>('/image-news', {
-      body,
-    })
+  async single(urlKey: keyof typeof UploadPath, body: FormData) {
+    const { data } = await useHdFetch<ResponseApi.FileSingle>(
+      UploadPath[urlKey],
+      { body }
+    )
     const path = data.value?.path || undefined
     return { path }
   },
---
+++ src/shared/enums/upload-path-url.ts
@@ -0,0 +1,9 @@
+export enum UploadPath {
+  NEWS = '/image-news',
+  AVATAR = '/image-avatar',
+  GALLERY = '/image-gallery',
+  PROGRAMS = '/image-programs',
+  CATEGORY = '/image-category',
+  DELETE_IMAGE = '/image-delete',
+  DELETE_FOLDER = '/folder-delete',
+}
