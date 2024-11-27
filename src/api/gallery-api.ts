d2d54ece02c8e1ec5b7a8b672ea2a5e6c9547eb6
Hardart <kaino@bk.ru>
2024-11-27 23:57:53 +0300

    feat: add new API method to gallery api

--- src/api/gallery-api.ts
+++ src/api/gallery-api.ts
@@ -1,9 +1,20 @@
 import type { ResponseApi } from '@/shared/types/ResponseAPI'
 import { useHdFetch } from './base-fetch/base-fetch'
+import { toValue, type MaybeRef } from 'vue'
+import type { Slide } from '@/shared/schemes/slide-schema'
 
 export const galleryAPI = {
   async list() {
     const { data } = await useHdFetch<ResponseApi.SlideList>('/gallery')
+    return data.value?.slides || []
+  },
+
+  async saveAllSlides(body: MaybeRef<Slide[]>) {
+    body = toValue(body)
+    const { data } = await useHdFetch<ResponseApi.SlideList>(
+      '/gallery-update',
+      { body }
+    )
     return data.value?.slides || []
   },
 }
