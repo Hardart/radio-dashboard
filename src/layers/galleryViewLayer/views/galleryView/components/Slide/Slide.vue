b2c0e3fff32737ccc5f901b8684a95f86308d8b9
Hardart <kaino@bk.ru>
2024-11-27 23:57:16 +0300

    feat: add new Slide component for gallery view

---
+++ src/layers/galleryViewLayer/views/galleryView/components/Slide/Slide.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import HdButton from '@/components/ui/hdButton/hdButton.vue'
+import HdInput from '@/components/ui/hdInput/hdInput.vue'
+import HdUploadImage from '@/components/ui/hdUploadImage/hdUploadImage.vue'
+import type { Slide } from '@/shared/schemes/slide-schema'
+
+defineProps<{
+  slide: Slide
+}>()
+defineEmits(['onAdd', 'onEdit', 'onCancel'])
+</script>
+
+<template>
+  <div class="slide">
+    <div class="slide__header">
+      <div class="slide__media">
+        <img v-if="slide.src" class="slide__image" :src="slide.src" alt="" />
+        <div v-else class="slide__placeholder"></div>
+        <div class="slide__controllers">
+          <HdUploadImage
+            name="GALLERY"
+            icon="image-add"
+            v-model="slide.src"
+            v-tooltip="{ label: 'загрузить изображение' }"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="slide__body">
+      <HdInput placeholder="Заголовок" v-model="slide.title" />
+      <HdInput placeholder="Описание" v-model="slide.subtitle" />
+      <HdInput placeholder="Ссылка" v-model="slide.to" />
+    </div>
+    <div class="slide__footer">
+      <HdButton
+        text="Отменить"
+        type="outline-danger"
+        @click="$emit('onCancel', slide)"
+      />
+
+      <HdButton
+        v-if="slide.id"
+        text="Изменить"
+        type="outline-success"
+        @click="$emit('onEdit', slide)"
+      />
+      <HdButton
+        v-else
+        text="Добавить"
+        type="outline-success"
+        @click="$emit('onAdd', slide)"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
