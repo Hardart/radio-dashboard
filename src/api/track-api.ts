0a1414c216ec1f165e5a896573715a9c8d7a4f64
Hardart <kaino@bk.ru>
2024-12-04 21:05:04 +0300

    feat: add new tracks API

---
+++ src/api/track-api.ts
@@ -0,0 +1,22 @@
+import type { ITunesResponse } from '@/shared/types/itunes'
+import { useHdFetch } from './base-fetch/base-fetch'
+import type { ResponseApi } from '@/shared/types/ResponseAPI'
+import { ofetch } from 'ofetch'
+
+export const trackAPI = {
+  async list() {
+    const { data } = await useHdFetch<ResponseApi.TrackList>('/track-list')
+    return data.value?.tracks || []
+  },
+
+  async getITunesMetadata(term: string) {
+    const url = 'https://itunes.apple.com/search'
+    const options = {
+      query: { term, limit: '10', entity: 'song', media: 'music' },
+      parseResponse: JSON.parse,
+    }
+
+    const response = await ofetch<ITunesResponse>(url, options)
+    return response.results.length ? response.results : null
+  },
+}
