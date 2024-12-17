71b62f91e47ecd81ab01bc5fcf5e95cebd49ddf3
Hardart <kaino@bk.ru>
2024-12-17 23:42:28 +0300

    refactor: add tooltips to contacts header controls

--- src/layers/settingsViewLayer/components/headerBar/HeaderBar.vue
+++ src/layers/settingsViewLayer/components/headerBar/HeaderBar.vue
@@ -10,9 +10,24 @@ defineEmits(['toggle-phone', 'toggle-mail', 'toggle-address'])
       <h3 class="dashboard__header-title">Настройки</h3>
     </div>
     <div class="dashboard__header--right">
-      <HdButton icon="phone-add" square @click="$emit('toggle-phone')" />
-      <HdButton icon="mail" square @click="$emit('toggle-mail')" />
-      <HdButton icon="address-add" square @click="$emit('toggle-address')" />
+      <HdButton
+        icon="phone-add"
+        square
+        @click="$emit('toggle-phone')"
+        v-tooltip="{ label: 'добавить телефон' }"
+      />
+      <HdButton
+        icon="mail"
+        square
+        @click="$emit('toggle-mail')"
+        v-tooltip="{ label: 'добавить почту' }"
+      />
+      <HdButton
+        icon="address-add"
+        square
+        @click="$emit('toggle-address')"
+        v-tooltip="{ label: 'добавить адрес' }"
+      />
     </div>
   </DashboardContentHeaderLayout>
 </template>
