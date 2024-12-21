c9849bfa4ef737b3a9f527dffcff200abd0e6931
Hardart <kaino@bk.ru>
2024-12-21 19:27:11 +0300

    fixup! refactor: remove auth state from auth store

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-import { RouterView, useRoute } from 'vue-router'
+import { RouterView } from 'vue-router'
 import DashboardLayout from '@/layouts/dashboardLayout.vue'
 import LoginLayout from '@/layouts/loginLayout.vue'
-import { computed } from 'vue'
 import HdTooltip from './components/ui/hdTooltip/hdTooltip.vue'
 import HdNotifications from './components/ui/hdNotification/hdNotifications.vue'
 import { useAuthStore } from './store/useAuthStore'
 
-const route = useRoute()
-const isLoginPage = computed(() => route.name === 'login')
-useAuthStore().loginAuto()
+const authStore = useAuthStore()
+authStore.loginAuto()
 </script>
 
 <template>
-  <component :is="isLoginPage ? LoginLayout : DashboardLayout">
+  <component :is="!authStore.isAuth ? LoginLayout : DashboardLayout">
     <RouterView />
   </component>
-  <HdTooltip v-if="!isLoginPage" />
+  <HdTooltip v-if="authStore.isAuth" />
   <HdNotifications />
 </template>
