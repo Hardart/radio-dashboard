c9400d0ded9196911e2e04abc4070b3b3672eef9
Hardart <kaino@bk.ru>
2025-02-09 21:44:26 +0300

    feat: add editor controls enum

---
+++ src/shared/enums/editor-controls.ts
@@ -0,0 +1,11 @@
+export enum EditorControls {
+  heading = 'heading',
+  bold = 'bold',
+  italic = 'italic',
+  underline = 'underline',
+  blockquote = 'blockquote',
+  floatLeft = 'floatLeft',
+  floatRight = 'floatRight',
+  textWrap = 'textWrap',
+  uploadImage = 'uploadImage',
+}
