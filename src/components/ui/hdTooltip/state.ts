02859bf632312e1a0bf9fc4de79c6f61f9f6150c
Hardart <kaino@bk.ru>
2025-02-15 13:56:48 +0300

    refactor: change position of tooltip on screen left side

--- src/components/ui/hdTooltip/state.ts
+++ src/components/ui/hdTooltip/state.ts
@@ -17,9 +17,12 @@ export const coords = reactive<Coords>({
 export const isShowTooltip = ref(false)
 
 export function showTooltip($target: HTMLElement, title: string | null) {
-  const { top, left, width } = $target.getBoundingClientRect()
-  coords.top = top - 15
-  coords.left = left + width / 2
-  coords.title = title || 'Tooltip'
+  const { top, left, width, height } = $target.getBoundingClientRect()
+  const rightBorder = left + width
+  const screenWidth = document.body.clientWidth
+
+  coords.top = top < 20 ? top + height + 15 : top - 15
+  coords.left = screenWidth - rightBorder < 30 ? left : left + width / 2
+  coords.title = title || 'Не указано'
   isShowTooltip.value = true
 }
