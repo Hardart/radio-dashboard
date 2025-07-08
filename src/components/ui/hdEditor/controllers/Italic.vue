fa42551cbe1ff345bb370474042a724e8b06dc49
Hardart <kaino@bk.ru>
2025-07-09 01:35:17 +0300

    chore: move hdEditor in UI folder

---
+++ src/components/ui/hdEditor/controllers/Italic.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import HdButton from '@/components/ui/hdButton/hdButton.vue'
+import { inject } from 'vue'
+
+const editor = inject<any>('tiptap')
+</script>
+
+<template>
+  <HdButton
+    square
+    icon="format-italic"
+    class="hd-editor__button"
+    :class="{ 'hd-editor__button--active': editor.isActive('italic') }"
+    @click="editor.chain().focus().toggleItalic().run()"
+    v-tooltip="{ label: 'наклонный текст' }"
+  />
+</template>
---
+++ src/components/ui/hdEditor/controllers/TextWrap.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import HdButton from '@/components/ui/hdButton/hdButton.vue'
+import { inject } from 'vue'
+
+const editor = inject<any>('tiptap')
+</script>
+
+<template>
+  <HdButton
+    square
+    icon="format-text-wrap"
+    class="hd-editor__button"
+    @click="editor.chain().focus().setHardBreak().run()"
+    v-tooltip="{ label: 'разрыв строки' }"
+  />
+</template>
