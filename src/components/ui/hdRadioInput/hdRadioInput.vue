43537ec6eb8dc300ddccbc49e64f2ece57d8c885
Hardart <kaino@bk.ru>
2025-07-09 01:41:55 +0300

    feat: create new Radio input component

---
+++ src/components/ui/hdRadioInput/hdRadioInput.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const option = defineModel({ required: true })
+defineProps<{
+  name?: string
+  label: string
+  value: string
+}>()
+</script>
+
+<template>
+  <label class="radio">
+    <input class="radio__input" type="radio" :name :value v-model="option" />
+    <span class="radio__icon"></span>
+    <span class="radio__content">{{ label }}</span>
+  </label>
+</template>
+
+<style lang="scss" scoped src="./hdRadioInput.scss" />
