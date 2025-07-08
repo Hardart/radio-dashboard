4bf524e0a7deafb72c3548ff99a62b32d4e09510
Hardart <kaino@bk.ru>
2025-07-09 02:01:09 +0300

    feat: add new HostList and HostItem components

---
+++ src/layers/settingsViewLayer/components/hostList/components/hostItem/hostItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { correctImageUrl } from '@/shared/helpers/utils'
+import type { User } from '@schema/user-schema'
+
+defineProps<{ user: User }>()
+</script>
+
+<template>
+  <li class="host">
+    <div class="host__media">
+      <img
+        :src="correctImageUrl(user.avatar)"
+        v-if="user.avatar"
+        class="host__avatar"
+        alt=""
+      />
+    </div>
+    <div class="host__name">{{ user.fullName }}</div>
+    <div class="host__roles">Роли - {{ user.roles.join(', ') }}</div>
+  </li>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
---
+++ src/layers/settingsViewLayer/components/hostList/hostList.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import type { User } from '@schema/user-schema'
+import HostItem from './components/hostItem/hostItem.vue'
+defineProps<{ users: User[] }>()
+defineEmits(['on-edit'])
+</script>
+
+<template>
+  <ul class="host-list">
+    <HostItem
+      v-for="user in users"
+      :user
+      :key="user.id"
+      @click="$emit('on-edit', user)"
+    />
+  </ul>
+</template>
+
+<style lang="scss" scoped src="./styles.scss" />
