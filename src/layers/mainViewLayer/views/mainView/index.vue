5d2772edfc84f3133fd1f027bc31503e03af1d11
Hardart <kaino@bk.ru>
2025-07-09 17:03:35 +0300

    fixup! refactor: small updates and changes

--- src/layers/mainViewLayer/views/mainView/index.vue
+++ src/layers/mainViewLayer/views/mainView/index.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-import SettingsContactsForm from '@/components/settings/SettingsContactsForm.vue'
-import HdButton from '@/components/ui/hdButton/hdButton.vue'
-import HdFormGroup from '@/components/ui/hdFormGroup/HdFormGroup.vue'
-import HdInput from '@/components/ui/hdInput/hdInput.vue'
-import HdModal from '@/components/ui/hdModal/HdModal.vue'
-import Addresses from '@/layers/settingsViewLayer/components/addresses/Addresses.vue'
-import Mails from '@/layers/settingsViewLayer/components/mails/Mails.vue'
-import DashboardContentFooterLayout from '@/layouts/dashboardContentFooterLayout.vue'
-import DashboardContentHeaderLayout from '@/layouts/dashboardContentHeaderLayout.vue'
-import DashboardContentLayout from '@/layouts/dashboardContentLayout.vue'
+import * as ContentLayout from '@contentLayout'
 </script>
 
 <template>
-  <DashboardContentLayout>
-    <DashboardContentHeaderLayout>
-      <div class="dashboard__header--left">
-        <h3 class="dashboard__header-title">Главная</h3>
-      </div>
-    </DashboardContentHeaderLayout>
-
-    <DashboardContentFooterLayout> </DashboardContentFooterLayout>
-  </DashboardContentLayout>
+  <ContentLayout.Root>
+    <ContentLayout.Header title="Главная" />
+  </ContentLayout.Root>
 </template>
-
-<style lang="scss" src="./style.scss" />
